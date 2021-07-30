365ca2976c216d0b3671512193d21e58f5446cce
Mostafa Nafie <MostafaTarekNafie@gmail.com>
2021-07-30 23:02:14 +0200

    Install and config passport

---
+++ app/Http/Controllers/API/V1/Auth/LoginController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Auth;
+
+use AElnemr\RestFullResponse\CoreJsonResponse;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Laravel\Passport\Exceptions\OAuthServerException;
+use Laravel\Passport\Http\Controllers\AccessTokenController;
+use Psr\Http\Message\ServerRequestInterface;
+use Nyholm\Psr7\Response as Psr7Response;
+use League\OAuth2\Server\Exception\OAuthServerException as LeagueException;
+
+class LoginController extends AccessTokenController
+{
+    use CoreJsonResponse;
+
+    public function issueToken(ServerRequestInterface $request)
+    {
+        $response = $this->withErrorHandling(function () use ($request) {
+            return $this->convertResponse(
+                $this->server->respondToAccessTokenRequest($request, new Psr7Response)
+            );
+        });
+
+        $tokenData = json_decode($response->getContent(), true);
+
+        return $this->ok($tokenData);
+    }
+
+    protected function withErrorHandling($callback)
+    {
+        try {
+            return $callback();
+        } catch (LeagueException $e) {
+            throw new OAuthServerException(
+                $e,
+                $this->convertResponse($e->generateHttpResponse(new Psr7Response))
+            );
+        }
+    }
+}

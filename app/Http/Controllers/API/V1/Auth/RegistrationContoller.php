f1177956384853ae909ba8ab113bff72d361c0a5
Mostafa Nafie <MostafaTarekNafie@gmail.com>
2021-08-01 22:38:44 +0200

    Use DB transaction

--- app/Http/Controllers/API/V1/Auth/RegistrationContoller.php
+++ app/Http/Controllers/API/V1/Auth/RegistrationContoller.php
@@ -9,14 +9,18 @@ use App\Http\Resources\UserResource;
 use App\Models\User;
 use App\Models\UserProfile;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class RegistrationContoller extends Controller
 {
     use CoreJsonResponse;
 
     public function register(RegistrationRequest $request) {
-        $user = User::create($request->only(['name', 'email', 'password']));
-        $user->profile()->create($request->only(['bio']));
+        $user = DB::transaction(function () use ($request) {
+            $user = User::create($request->only(['name', 'email', 'password']));
+            $user->profile()->create($request->only(['bio']));
+            return $user;
+        });
 
         return $this->created((new UserResource($user))->resolve());
     }
